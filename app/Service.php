fc145b61ec412bbda2ae5eb04cbb9cc9a22c7c2f
c-neri <cristiano.piscioneri@gmail.com>
2019-08-29 09:42:22 +0200

    Db seeder-services

--- app/Service.php
+++ app/Service.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    public $timestamps = false;
     public function apartments() {
       return $this->belongsToMany('App\Apartment');
     }
---
+++ database/seeds/ServicesTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Service;
+class ServicesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $services = [
+        'basic',
+        'wifi',
+        'aria condizionata',
+        'terrazzo',
+        'cusina',
+        'appendiabiti',
+        'ferrodastiro',
+        'asciugacapelli',
+        'colazione',
+        'acqua calda',
+        'riscaldamento',
+        'asciugatrice',
+        'rilevatore di fumo',
+      ];
+      foreach ($services as $single_service) {
+        $newservice = new Service;
+        $newservice->name = $single_service;
+        $newservice->save();
+      }
+    }
+}
