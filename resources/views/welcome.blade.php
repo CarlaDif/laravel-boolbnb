1014fe0ba964c7845dff28e16cfad85cee77dd92
CarlaDif <carlita4692@gmail.com>
2019-09-06 23:06:42 +0200

    style card welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,9 +91,9 @@
           >
           <div class="card mt-3" style="width: 16rem;">
             @if (!empty($apartment->main_img))
-              <img src="{{ asset('storage/' . $apartment->main_img) }}" class="card-img-top" style="width: 250px; height: 150px; object-fit: cover;" alt="Anteprima Appartamento">
+              <img src="{{ asset('storage/' . $apartment->main_img) }}" class="card-img-top" style="width: 100%; height: 150px; object-fit: cover;" alt="Anteprima Appartamento">
             @else
-              <img style="width: 250px; height: 150px; object-fit: cover;" src="https://kitv.images.worldnow.com/images/16468883_G.png?lastEditedDate=1522902908000" class="card-img-top" alt="Anteprima non disponibile">
+              <img style="width: 100%; height: 150px; object-fit: cover;" src="https://kitv.images.worldnow.com/images/16468883_G.png?lastEditedDate=1522902908000" class="card-img-top" alt="Anteprima non disponibile">
             @endif
               <div class="card-body">
                 <h5 class="card-title">{{ $apartment->title }}</h5>
